89d9f3c750852e1866d1478047ceda7afc999ff5
Arbaz-Apsis <arbaz.shaikh@apsissolutions.com>
2024-04-15 15:39:57 +0530

    Worked on responsivness of career page.

--- frontend/src/app/(website)/career/page.tsx
+++ frontend/src/app/(website)/career/page.tsx
@@ -39,7 +39,7 @@ export default function Career() {
         </div>
       </section>
       <section className=" py-12">
-        <div className="my-12 desktop:container m-auto grid laptop:grid-cols-2 desktop:grid-cols-4 tablet:grid-cols-2 gap-5">
+        <div className="my-12 desktop:container laptop:container m-auto grid laptop:grid-cols-2 desktop:grid-cols-4 tablet:grid-cols-2 gap-5">
           <div className="desktop:w-full bg-gray-100 mx-4 text-center p-4 rounded-md shadow-lg">
             <h2 className="font-bold text-2xl">Diversity</h2>
             <p className="text-lg">
@@ -73,93 +73,99 @@ export default function Career() {
           </div>
         </div>
 
-        <div className="flex justify-between container mb-8 m-auto">
-          <div className="w-1/5">
+        <div className="flex justify-between desktop:flex-row laptop:flex-row flex-col desktop:container laptop:container mb-8 m-auto">
+          <div className="desktop:w-1/5 laptop:w-1/5 w-full mx-4">
             <div>
-              <form action="" className="bg-gray-100 p-2 flex flex-col ">
-                <h2 className="font-bold mb-2 ">Department</h2>
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="border  p-2"
-                />
-                <br />
-                <div>
-                  <input type="checkbox" id="accounting" />
-                  <label htmlFor="accounting">Accounting</label>
+              <form
+                action=""
+                className="bg-gray-100 p-2 flex flex-col items-center desktop:flex-col laptop:flex-col tablet:flex-row tablet:justify-start tablet:items-start"
+              >
+                <div className="tablet:w-1/2 desktop:w-full laptop:w-full">
+                  <h2 className="font-bold mb-2 ">Department</h2>
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    className="border desktop:w-full laptop:w-full p-2"
+                  />
                   <br />
+                  <div>
+                    <input type="checkbox" id="accounting" />
+                    <label htmlFor="accounting">Accounting</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="cerdentials-&-content" />
+                    <label htmlFor="cerdentials-&-content">
+                      Cerdentials & Content
+                    </label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="data-science" />
+                    <label htmlFor="data-science">Data Science</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="degree-marketing" />
+                    <label htmlFor="degree-marketing">Degree Marketing</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="IT" />
+                    <label htmlFor="it">it</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="marketing" />
+                    <label htmlFor="marketing">Marketing</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="product-&-content" />
+                    <label htmlFor="product-&-content">Product & Content</label>
+                    <br />
+                    <br />
+                    <br />
+                  </div>
                 </div>
-                <div>
-                  <input type="checkbox" id="cerdentials-&-content" />
-                  <label htmlFor="cerdentials-&-content">
-                    Cerdentials & Content
-                  </label>
+                <div className="tablet:w-1/2 desktop:w-full laptop:w-full">
+                  <h2 className="font-bold mb-2 ">Country</h2>
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    className="border desktop:w-full laptop:w-full p-2"
+                  />
                   <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="data-science" />
-                  <label htmlFor="data-science">Data Science</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="degree-marketing" />
-                  <label htmlFor="degree-marketing">Degree Marketing</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="IT" />
-                  <label htmlFor="it">it</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="marketing" />
-                  <label htmlFor="marketing">Marketing</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="product-&-content" />
-                  <label htmlFor="product-&-content">Product & Content</label>
-                  <br />
-                  <br />
-                  <br />
-                </div>
-
-                <h2 className="font-bold mb-2 ">Country</h2>
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="border  p-2"
-                />
-                <br />
-                <div>
-                  <input type="checkbox" id="cnanda" />
-                  <label htmlFor="cnanda">Cnanda</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="colombia" />
-                  <label htmlFor="colombia">Colombia</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="india" />
-                  <label htmlFor="india">India</label>
-                  <br />
-                </div>
-                <div>
-                  <input type="checkbox" id="united-state" />
-                  <label htmlFor="united-state">United State</label>
-                  <br />
+                  <div>
+                    <input type="checkbox" id="cnanda" />
+                    <label htmlFor="cnanda">Cnanda</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="colombia" />
+                    <label htmlFor="colombia">Colombia</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="india" />
+                    <label htmlFor="india">India</label>
+                    <br />
+                  </div>
+                  <div>
+                    <input type="checkbox" id="united-state" />
+                    <label htmlFor="united-state">United State</label>
+                    <br />
+                  </div>
                 </div>
               </form>
             </div>
           </div>
-          <div className="w-3/4">
-            <div className="mb-8">
+          <div className="desktop:w-3/4 laptop:w-3/4 w-full mx-4">
+            <div className="mb-8 flex justify-center desktop:justify-start laptop:justify-start items-center mt-2 desktop:mt-0 laptop:mt-0">
               <input
                 type="text"
                 placeholder="Search by job title, location, department, catagory, etc."
-                className="w-3/4 border-2 p-2 mr-2"
+                className=" border-2 p-2 mr-2 w-[60%] laptop:w-[63%]"
               />
               <button className="bg-blue-600 py-2 px-4 border text-white rounded-md hover:bg-white hover:border-blue-700 hover:text-blue-700 mr-2">
                 Search
@@ -169,6 +175,23 @@ export default function Career() {
               </button>
             </div>
             <div>
+              {/* <div className="tablet:w-1/2 desktop:w-full laptop:w-full">
+                <h3 className="text-start m-1">Title</h3>
+                <p className="text-start m-1">Staff Accountant</p>
+                <p className="text-start m-1">
+                  Data Science Content Strategist
+                </p>
+                <p className="text-start m-1">SaaS Intigration Specialist</p>
+                <p className="text-start m-1">ServiceNow Developer</p>
+                <p className="text-start m-1">Chief of Staff-CRO</p>
+                <p className="text-start m-1">Machine Learning Engineer</p>
+                <p className="text-start m-1">Enrollment Counselor, LATAM</p>
+                <p className="text-start m-1">Manager, Enrollment Services</p>
+                <p className="text-start m-1">
+                  Staff ML Scientist (Recommender System)
+                </p>
+                <p className="text-start m-0">Senior Tax Analyst</p>
+              </div> */}
               <table className="w-full">
                 <tr>
                   <th className="text-start">Title</th>
