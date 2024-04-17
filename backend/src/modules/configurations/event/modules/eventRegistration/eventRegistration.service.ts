6a213639da8cdcfa99c0a185946f9fd5c9b5aa98
Umair-rinde <rindeumair@gmail.com>
2024-04-17 12:19:57 +0530

    path for excel is fixed for event-webinar registration

--- backend/src/modules/configurations/event/modules/eventRegistration/eventRegistration.service.ts
+++ backend/src/modules/configurations/event/modules/eventRegistration/eventRegistration.service.ts
@@ -5,7 +5,7 @@ import { EventRegistrationdto } from './dto/create-eventRegistration.dto';
 import { UpdateEventRegistrationDto } from './dto/update-eventRegiter.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import * as XLSX from 'xlsx';
-import { join } from 'path';
+import { join, relative } from 'path';
 import { existsSync, mkdirSync, readFileSync } from 'fs';
 import { MailsService } from 'src/common/modules';
 import { MailerServices } from 'src/modules/configurations/Enquiry/mail/mail.service';
@@ -60,28 +60,39 @@ export class EventRegistrationService extends GenericService<
   async exportToExcel(data: any, filename: string): Promise<string> {
     try {
       if (!data || !Array.isArray(data.rows) || data.rows.length === 0) {
-        throw new Error('Invalid data format.');
+        return 'No registrations yet';
       }
-
+  
       const registrations: EventRegistration[] = data.rows;
-      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(registrations.map(reg => reg.dataValues));
-      const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-      const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
-
-      const directory = join(process.cwd(), 'src','public', 'event-registration');
+      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(
+        registrations.map(reg => reg.dataValues),
+      );
+      const workbook: XLSX.WorkBook = {
+        Sheets: { data: worksheet },
+        SheetNames: ['data'],
+      };
+      const excelBuffer: any = XLSX.write(workbook, {
+        bookType: 'xlsx',
+        type: 'buffer',
+      });
+  
+      const directory = join(process.cwd(), 'src', 'public', 'event-registration');
       if (!existsSync(directory)) {
         mkdirSync(directory, { recursive: true });
       }
-
+  
       const filePath = join(directory, `${filename}.xlsx`);
       require('fs').writeFileSync(filePath, excelBuffer);
-      
-      console.log(`Excel file '${filename}.xlsx' successfully exported to '${directory}'.`);
-      
-      return filePath;
+  
+      // Convert the absolute path to a relative path
+      const relativePath = relative(join(process.cwd(), 'src', 'public'), filePath).replace(/\\/g, '/');
+      console.log(`Excel file '${filename}.xlsx' successfully exported to '${relativePath}'.`);
+  
+      return relativePath;
     } catch (error) {
       console.error(`Error exporting Excel file: ${error.message}`);
       throw error;
     }
   }
+  
 }
