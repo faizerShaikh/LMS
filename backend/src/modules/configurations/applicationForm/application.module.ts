a36bc4a2b4b90ecc96b529bbb5e91b315fab3288
Umair-Rinde <rindeumair@gmail.com>
2024-04-23 11:10:04 +0530

    updated application service

--- backend/src/modules/configurations/applicationForm/application.module.ts
+++ backend/src/modules/configurations/applicationForm/application.module.ts
@@ -4,9 +4,10 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { ApplicationService } from "./application.service";
 import { ApplicationForm } from "./application.model";
 import { ApplicationController } from "./appplication.controller";
+import { MailModule } from "../Enquiry/mail/mail.module";
 
 @Module({
-  imports: [SequelizeModule.forFeature([ApplicationForm])],
+  imports: [SequelizeModule.forFeature([ApplicationForm]),MailModule],
   controllers: [ApplicationController],
   providers: [ApplicationService]
 })
