cea1aacacaec7f47f218dd655cd24a066de49794
Umair-Rinde <rindeumair@gmail.com>
2024-07-10 17:56:41 +0530

    minor fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -42,15 +42,15 @@ import { JwtAuthGuard } from './core/guards';
     //   })
     // })
   ],
-  // providers: [
-  //   {
-  //     provide: APP_INTERCEPTOR,
-  //     useClass: ResponseInterceptor,
-  //   },
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
   //   {
   //     provide: APP_GUARD,
   //     useClass: JwtAuthGuard,
   //   },
-  // ],
+  ],
 })
 export class AppModule {}
